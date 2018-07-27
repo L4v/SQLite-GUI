c47b938b752af0c5fc4986c3b291102690974d76
l4v <ivos.jovan@gmail.com>
2018-07-27 18:14:19 +0200

    first commit

---
+++ SQLite GUI/SQLite GUI/Database.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace SQLite_GUI
+{
+
+
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Database(String database_name) {
+            // Creates a new connection
+            myConnection = new SQLiteConnection("Data Source="+ database_name);
+
+            // Create a new database file if one doesn't exist
+            if (!File.Exists("./"+database_name))
+                 SQLiteConnection.CreateFile(database_name);
+        }
+
+        /// <summary>
+        /// If the database connection is closed, open it
+        /// </summary>
+        public void OpenConnection() {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+                myConnection.Open();
+            return;
+        }
+
+        /// <summary>
+        /// If the database connection is open, close it
+        /// </summary>
+        public void CloseConnection()
+        {
+            if (myConnection.State == System.Data.ConnectionState.Open)
+                myConnection.Close();
+            return;
+        }
+
+    }
+}
---
+++ SQLite GUI/SQLite GUI/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Windows;
+using System.Data.SQLite;
+using System.Data;
+using System.Windows.Documents;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SQLite_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region Private helpers
+        // Create new Database object
+        private Database database = new Database("test.sqlite3");
+        private DataTable dataTable;
+        private SQLiteDataAdapter dataAdapter;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NewGUI();
+        }
+
+        /// <summary>
+        /// Creates a new instance of a GUI
+        /// </summary>
+        private void NewGUI()
+        {
+            // Clears input text
+            this.InputText.Document.Blocks.Clear();
+
+            // Clear message label
+            this.MessageLabel.Content = String.Empty;
+
+            // Update tables list
+            UpdateTableList();
+
+        }
+        #endregion
+
+        #region clicks and such
+        /// <summary>
+        /// Processes command inside textbox
+        /// </summary>
+        /// <param name="sender">Button which was clicked</param>
+        /// <param name="e">Event object</param>
+        private void RunButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open connection
+            database.OpenConnection();
+
+            // Create new data table
+            dataTable = new DataTable();
+
+            // Input text to make a commands
+            string query = new TextRange(InputText.Document.ContentStart, InputText.Document.ContentEnd).Text;
+
+            // Create new data adapter
+            dataAdapter = new SQLiteDataAdapter(query, database.myConnection);
+
+            // Fill table
+            dataAdapter.Fill(dataTable);
+            
+            // Sets default view for the table
+            OutputGrid.ItemsSource = dataTable.DefaultView;
+
+            // Sample message
+            this.MessageLabel.Content = "Command executed";
+
+            UpdateTableList();
+            
+            // Close connection
+            database.CloseConnection();
+        }
+
+        /// <summary>
+        /// Shows selected table in ListBox 
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">Event object</param>
+        private void TablesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Take the name from selected item in ListBox, the name starts at the 37th character
+            string name = this.TablesList.SelectedItem.ToString().Substring(37);
+            ShowTable(name);
+        }
+        #endregion
+
+        /// <summary>
+        /// Update tables list
+        /// </summary>
+        private void UpdateTableList() {
+            // Open connection
+            database.OpenConnection();
+
+            // Create new connection
+            SQLiteConnection connection = new SQLiteConnection(database.myConnection);
+
+            // Sqlite data reader
+            SQLiteDataReader reader = GetCommand("SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY 1").ExecuteReader();
+
+            // Clear list
+            this.TablesList.Items.Clear();
+
+            ListBoxItem item;
+
+
+            // TODO: DODATI DOUBLE CLICK NA LIST ITEM
+
+            while (reader.Read())
+            {
+                item = new ListBoxItem();
+                item.Content = (string)reader["name"];
+                TablesList.Items.Add(item);
+            }
+
+            // Close connection
+            database.CloseConnection();
+        }
+
+        /// <summary>
+        /// Shows a table if exists
+        /// </summary>
+        /// <param name="name">Name of table to show</param>
+        private void ShowTable(string name) {
+            // Open the connection
+            database.OpenConnection();
+
+            string query = "SELECT * FROM " + name; 
+
+            // Create new DataTable
+            dataTable = new DataTable();
+
+            // Create new data adapter
+            dataAdapter = new SQLiteDataAdapter(query, database.myConnection);
+
+            // Fill table
+            dataAdapter.Fill(dataTable);
+
+            // Sets default view for the table
+            OutputGrid.ItemsSource = dataTable.DefaultView;
+
+            // Info message
+            this.MessageLabel.Content = "Showing table...";
+
+            // Close the connection
+            database.CloseConnection();
+        }
+
+        #region getters
+
+        /// <summary>
+        /// Takes a query as a string and returns a SQLite command object
+        /// </summary>
+        /// <param name="query">Query as a string for the command</param>
+        /// <returns></returns>
+        private SQLiteCommand GetCommand(string query)
+        {
+            SQLiteCommand command = new SQLiteCommand(query, database.myConnection);
+
+            return command;
+        }
+
+        #endregion
+
+    }
+}
